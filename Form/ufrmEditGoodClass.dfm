527f1d830fbc5a0af6d9b5474fddf5d62c5c19b5
zhyjun@163.com <zhyjun>
2020-08-29 01:38:00 +0800

    增加商品分类编辑增加模块

---
+++ Form/ufrmEditGoodClass.dfm
@@ -0,0 +1,15 @@
+inherited frmEditGoodClass: TfrmEditGoodClass
+  Caption = #21830#21697#20998#31867'['#22686#21152']'
+  PixelsPerInch = 96
+  TextHeight = 13
+  inherited P1: TPanel
+    inherited cxPageControl1: TcxPageControl
+      inherited cxTabSheet1: TcxTabSheet
+        ExplicitLeft = 3
+        ExplicitTop = 33
+        ExplicitWidth = 862
+        ExplicitHeight = 356
+      end
+    end
+  end
+end
---
+++ Form/ufrmEditGoodClass.pas
@@ -0,0 +1,40 @@
+unit ufrmEditGoodClass;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, ufrmEditBase, dxSkinsCore, dxSkinBlack,
+  dxSkinBlue, dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee, dxSkinDarkroom,
+  dxSkinDarkSide, dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy,
+  dxSkinGlassOceans, dxSkinHighContrast, dxSkinLilian, dxSkinLiquidSky,
+  dxSkinLondonLiquidSky, dxSkinMcSkin, dxSkinMetropolis, dxSkinMetropolisDark,
+  dxSkinMoneyTwins, dxSkinOffice2007Black, dxSkinOffice2007Blue,
+  dxSkinOffice2007Green, dxSkinOffice2007Pink, dxSkinOffice2007Silver,
+  dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
+  dxSkinOffice2013DarkGray, dxSkinOffice2013LightGray, dxSkinOffice2013White,
+  dxSkinOffice2016Colorful, dxSkinOffice2016Dark, dxSkinPumpkin, dxSkinSeven,
+  dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus, dxSkinSilver,
+  dxSkinSpringtime, dxSkinStardust, dxSkinSummer2008, dxSkinTheAsphaltWorld,
+  dxSkinTheBezier, dxSkinValentine, dxSkinVisualStudio2013Blue,
+  dxSkinVisualStudio2013Dark, dxSkinVisualStudio2013Light, dxSkinVS2010,
+  dxSkinWhiteprint, dxSkinXmas2008Blue, dxBarBuiltInMenu, cxGraphics,
+  cxControls, cxLookAndFeels, cxLookAndFeelPainters, Vcl.Menus, Vcl.StdCtrls,
+  cxButtons, cxPC, Vcl.ExtCtrls;
+
+type
+  TfrmEditGoodClass = class(TfrmEditBase)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmEditGoodClass: TfrmEditGoodClass;
+
+implementation
+
+{$R *.dfm}
+
+end.
